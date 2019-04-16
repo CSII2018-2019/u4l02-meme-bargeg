e09c540edf14d6b5e152787e3743052a578d445c
gennabarge <gennabarge@10.0.0.10>
2019-04-16 10:31:31 -0400

    COMMIT for new comp

--- src/U4L02Code.java
+++ src/U4L02Code.java
@@ -1,8 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,23 +29,69 @@ public class U4L02Code extends JFrame {
 	
 	public void initGUI(){
 		//title Panel
+		ImagePanel imgPanel = new ImagePanel(FILE_NAME);
+		add(imgPanel, BorderLayout.LINE_START);
 		JPanel titlePanel = new JPanel();
 		titlePanel.setBackground(Color.BLACK);
 		JLabel titleLabel = new JLabel("Walking into a test you didn't study for like");
 		titleLabel.setForeground(Color.WHITE);
 		titlePanel.add(titleLabel);
 		add(titlePanel, BorderLayout.PAGE_START);
+		JPanel downButtonPanel = new JPanel();
+		downButtonPanel.setBackground(Color.LIGHT_GRAY);
+		add(downButtonPanel, BorderLayout.PAGE_END);
+		JButton conspiracyButton = new JButton ("conspiracy filter");
+		JButton grayscale = new JButton ("grayscale");
+		JButton rgbRed = new JButton ("rgb red filter");
+		JButton sepia = new JButton ("sepia filter");
+		conspiracyButton.setBackground(Color.WHITE);
+		grayscale.setBackground(Color.WHITE);
+		rgbRed.setBackground(Color.WHITE);
+		sepia.setBackground(Color.WHITE);
+		conspiracyButton.setForeground(Color.BLACK);
+		grayscale.setForeground(Color.DARK_GRAY);
+		rgbRed.setForeground(Color.RED);
+		sepia.setForeground(Color.YELLOW);
+		Font font = new Font ("PAPYRUS" , Font.BOLD + Font.ITALIC,  32);
+		conspiracyButton.setFont(font);
+		grayscale.setFont(font);
+		rgbRed.setFont(font);
+		sepia.setFont(font);
+		conspiracyButton.addActionListener(new ActionListener() {
+			public void actionPerformed (ActionEvent e) {
+				ImagePanel rgbImg = new ImagePanel(FILE_NAME);
+				rgbImg.conspiracyFilter();
+				add(rgbImg,BorderLayout.LINE_END);
+			}
+			public void actionPerformed (ActionEvent e) {
+				ImagePanel rgbImg = new ImagePanel(FILE_NAME);
+				rgbImg.conspiracyFilter();
+				add(rgbImg,BorderLayout.LINE_END);
+			}
+			public void actionPerformed (ActionEvent e) {
+				ImagePanel rgbImg = new ImagePanel(FILE_NAME);
+				rgbImg.conspiracyFilter();
+				add(rgbImg,BorderLayout.LINE_END);
+			}
+			public void actionPerformed (ActionEvent e) {
+				ImagePanel rgbImg = new ImagePanel(FILE_NAME);
+				rgbImg.conspiracyFilter();
+				add(rgbImg,BorderLayout.LINE_END);
+			}
+		}); 
+ 
+
+		downButtonPanel.add(conspiracyButton);
+		downButtonPanel.add(grayscale);
+		downButtonPanel.add(rgbRed);
+		downButtonPanel.add(sepia);
+	}
 		
 		//image panel
-		ImagePanel imgPanel = new ImagePanel(FILE_NAME);
-		add(imgPanel, BorderLayout.LINE_START);
 		
 		//grayscale image panel
-		ImagePanel rgbImg = new ImagePanel(FILE_NAME);
-		rgbImg.conspiracyFilter();
-		add(rgbImg,BorderLayout.LINE_END);
 
-	}
+	
 	
 	
 	
